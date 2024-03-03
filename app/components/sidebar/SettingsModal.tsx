3a02749248d58e76a128c4a8a13f21655d951727
Elias Nau <elias.nau@outlook.de>
2024-03-03 11:49:05 +0100

    Hot Fix

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -120,7 +120,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
                   <CldUploadButton
                     options={{ maxFiles: 1 }}
                     onUpload={handleUpload}
-                    uploadPreset="pgc9ehd5"
+                    uploadPreset="eagnbcwu"
                   >
                     <Button disabled={isLoading} secondary type="button">
                       Change
